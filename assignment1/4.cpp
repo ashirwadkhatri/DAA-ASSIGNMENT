ac4d85f59a4300cee98d006a6aeb29ffd656da3e
ashirwad khatri <khatriashirwad890@gmail.com>
2026-01-29 16:00:31 +0000

    added

---
+++ assignment1/4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {-2, -5, 6, -2, -3, 1, 5, -6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int currentSum = arr[0];
+    int maxSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currentSum = max(arr[i], currentSum + arr[i]);
+        maxSum = max(maxSum, currentSum);
+    }
+
+    cout << "Maximum subarray sum is: " << maxSum;
+
+    return 0;
+}
